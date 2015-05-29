5a6f7b16ed75fb71adfef085d311958ca731cff7
Torben Knerr <mail@tknerr.de>
2015-05-29 09:13:32 +0200

    add initial cookbook version, as generated via `chef generate cookbook jenkins-simple-app`

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'jenkins-simple-app'
+maintainer 'Torben Knerr'
+maintainer_email 'torben.knerr@zuehlke.com'
+license 'mit'
+description 'Installs/Configures jenkins-simple-app'
+long_description 'Installs/Configures jenkins-simple-app'
+version '0.1.0'
