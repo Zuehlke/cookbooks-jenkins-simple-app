cebdaaf38e1e95237d6767080dc3e22227104701
Torben Knerr <mail@tknerr.de>
2015-06-15 07:29:47 +0200

    add serverspec integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
 
 describe 'jenkins-simple-app::default' do
+  #
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  #
+  it 'starts the jenkins service' do
+    service('jenkins').should be_running
   end
+  it 'enables the jenkins service on startup' do
+    service('jenkins').should be_enabled
+  end
+  it 'runs jenkins on port 8080' do
+    port(8080).should be_listening
+  end
+
+  it 'installs jenkins version 1.609.1' do
+    cmd = command('wget -qO- localhost:8080')
+    cmd.stdout.should include 'Jenkins ver. 1.609.1'
+  end
+
 end
