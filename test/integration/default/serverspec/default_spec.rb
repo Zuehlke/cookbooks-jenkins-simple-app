95a0b842974faf9bed26e4993c2abf1139f4f375
Torben Knerr <mail@tknerr.de>
2015-06-15 07:52:10 +0200

    switch to the new and preferred expect syntax

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,19 +5,20 @@ describe 'jenkins-simple-app::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   #
+
   it 'starts the jenkins service' do
-    service('jenkins').should be_running
+    expect(service('jenkins')).to be_running
   end
   it 'enables the jenkins service on startup' do
-    service('jenkins').should be_enabled
+    expect(service('jenkins')).to be_enabled
   end
   it 'runs jenkins on port 8080' do
-    port(8080).should be_listening
+    expect(port(8080)).to be_listening
   end
 
   it 'installs jenkins version 1.609.1' do
     cmd = command('wget -qO- localhost:8080')
-    cmd.stdout.should include 'Jenkins ver. 1.609.1'
+    expect(cmd.stdout).to include 'Jenkins ver. 1.609.1'
   end
 
 end
