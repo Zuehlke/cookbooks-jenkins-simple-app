017070195b946dce66948cae36b57af3bd5e1147
Torben Knerr <mail@tknerr.de>
2015-06-19 02:47:34 +0200

    you always need to pass the url and checksum too if you want a specific version, so let's do that...

--- test/integration/java_slave/serverspec/java_slave_spec.rb
+++ test/integration/java_slave/serverspec/java_slave_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe 'jenkins-simple-app::java_slave' do
 
-  it 'installs oracle java 8' do
-    expect(command('java -version').stdout).to include '1.8'
+  it 'installs Oracle Java 8 update 31' do
+    expect(command('java -version').stdout).to include '1.8.0_31'
   end
 
   it 'sets JAVA_HOME correctly' do
     expect(command('bash -l -c "echo \$JAVA_HOME"').stdout).to include '/usr/lib/jvm/java-8-oracle-amd64'
   end
 
-  it 'installs Maven 3.3.3' do
-    expect(command('bash -l -c "mvn -v"').stdout).to include 'Apache Maven 3.3.3'
+  it 'installs Maven 3.1.1' do
+    expect(command('bash -l -c "mvn -v"').stdout).to include 'Apache Maven 3.1.1'
   end
 end
